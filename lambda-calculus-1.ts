25d7ee2421cf94761db1409de6d7163951f36e54
possum <possum@mail.com>
2021-07-19 00:27:01 +1000

    Identity function

---
+++ lambda-calculus-1.ts
@@ -0,0 +1,27 @@
+type A = {
+  property: string;
+}
+
+type B = {
+  property: number;
+}
+
+// A → B, read as a function from type A to type B
+type AtoB = {
+  (a: A): B; 
+}
+
+// Identity functions
+// id = λx.x
+const id = (x: A) => x
+// id = λ(x → y).(x → y)
+const id1 = (x: AtoB) => x
+
+const a_to_b_example: AtoB = ({property: a}) => {
+  return {
+    property: Number(a)
+  }
+}
+
+console.log(id({property: 'example'}))
+console.log(id1(a_to_b_example))
